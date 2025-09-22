5582fb0404dac43975d25146bc155e3fdbf65149
nahomaraya <nahomaraya8@gmail.com>
2025-09-21 17:06:16 -0700

    removed wikidata secrets to env

--- src/config/wikidata.config.ts
+++ src/config/wikidata.config.ts
@@ -1,8 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('wikidata', () => ({
-  // clientId: process.env.WIKIDATA_CLIENT_ID,
-  // clientSecret: process.env.WIKIDATA_CLIENT_SECRET,
-  clientId: "1355d0ca1f69c38ac0770985fbf367ac",
-  clientSecret: "080de0aa66e9a989f4b130c210c582a74be5dabd",
+  clientId: process.env.WIKIDATA_CLIENT_ID,
+  clientSecret: process.env.WIKIDATA_CLIENT_SECRET,
 }));
